415513dd27b75154d0c12051eb25e917fc49281d
lutfiqaraman <lutfiqaraman@gmail.com>
2021-04-08 21:45:25 +0300

    Add project files.

---
+++ BookShopApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookShopApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
