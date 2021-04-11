b8b63b4928ca2ae20e863ebd014011ed2235e5fb
lutfiqaraman <lutfiqaraman@gmail.com>
2021-04-11 15:57:34 +0300

    Create a tag class

---
+++ BookShop.Domain/Models/Tag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShop.Domain.Models
+{
+    public class Tag
+    {
+        public string TagId { get; set; }
+        public ICollection<Book> Books { get; set; }
+    }
+}
