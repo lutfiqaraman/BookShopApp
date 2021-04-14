18701d29dcfb6429fa14e194007f4671e7362bf6
lutfiqaraman <lutfiqaraman@gmail.com>
2021-04-14 21:37:44 +0300

    add entity called review

---
+++ BookShop.Domain/Models/Review.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShop.Domain.Models
+{
+    public class Review
+    {
+        public int ReviewId { get; set; }
+        public string VoterName { get; set; }
+        public int NumStart { get; set; }
+        public string Comment { get; set; }
+        public int BookId { get; set; }
+    }
+}
