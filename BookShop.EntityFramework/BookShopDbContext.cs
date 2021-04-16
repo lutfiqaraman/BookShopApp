031c0f4dcd137c2e3c676bf13d6234100ab63998
lutfiqaraman <lutfiqaraman@gmail.com>
2021-04-16 20:32:56 +0300

    Building BookShopDbContext class

--- BookShop.EntityFramework/BookShopDbContext.cs
+++ BookShop.EntityFramework/BookShopDbContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,11 @@ using System.Threading.Tasks;
 
 namespace BookShop.EntityFramework
 {
-    public class BookShopDbContext
+    public class BookShopDbContext : DbContext
     {
+        public BookShopDbContext(DbContextOptions<BookShopDbContext> options) : base(options)
+        {
 
+        }
     }
 }
